9e82d41e4b73001ff4dab66284b7223d3a58d40e
HenryD11703 <3680558330101@ingenieria.usac.edu.gt>
2024-09-23 14:45:00 -0600

    Add label element to task list items

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ const taskList = document.getElementById('task-list');
 
 boton.addEventListener('click', () => {
   const task = document.createElement('li');
+  const label = document.createElement('label');
   const checkbox = document.createElement('input');
   checkbox.type = 'checkbox';
-  task.appendChild(checkbox);
-  task.appendChild(document.createTextNode(taskInput.value));
+  label.appendChild(checkbox);
+  label.appendChild(document.createTextNode(taskInput.value));
+  task.appendChild(label);
   taskList.appendChild(task);
   taskInput.value = '';
 });
